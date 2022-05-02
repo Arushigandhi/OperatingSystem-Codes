f325351dae6f715bcf5a6e5b4cd5e0ca45d50440
Arushi Gandhi <75487137+Arushigandhi@users.noreply.github.com>
2022-05-02 08:59:31 +0530

    Update Lab7_1.c

--- Lab7_1.c
+++ Lab7_1.c
@@ -1,184 +1,51 @@
-#include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <semaphore.h>
 
-typedef struct
-{
-    int pid;
-    int arrival_time;
-    int burst_time;
-    int rem_time;
-    int priority;
-} Process;
+int capacity;
+int *queue;
+int max;
+sem_t full, empty;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-Process *Entry()
-{
-    printf("Enter the number of processes:");
-    int n;
-    scanf("%d", &n);
-    Process *p = (Process *)malloc(n + 1 * sizeof(Process));
-    printf("Enter PID, arrival, burst, Priority ( Enter 0 for default )\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("Process %d :", i + 1);
-        scanf("%d %d %d %d", &p[i].pid, &p[i].arrival_time, &p[i].burst_time, &p[i].priority);
-        p[i].rem_time = p[i].burst_time;
+void *producer( void * param){
+    int in = 0;
+    for ( int i=0; i<max; i++ ){
+        sem_wait(&empty);  //Wait for post on empty
+        pthread_mutex_lock( &mutex );
+        queue[in] = i+1;
+        in = (in+1)%capacity;
+        printf("Produced %d\n", i+1);
+        pthread_mutex_unlock( &mutex );
+        sem_post( &full );  //Signal full
     }
-    p[n].pid = -1;
-    return p;
 }
-
-int min(Process *p, int n, int timer)
-{
-    int min_time = 9999;
-    int min = -1;
-    for (int i = 0; i < n; i++)
-    {
-        if (p[i].arrival_time <= timer && p[i].rem_time > 0 && p[i].burst_time < min_time)
-        {
-            min_time = p[i].burst_time;
-            min = i;
-        }
+void *consumer(void * param){
+    int out = 0;
+    for ( int i=0; i<max; i++ ){
+        sem_wait(&full);  //Wait for post on full
+        pthread_mutex_lock( &mutex );
+        printf("Consumed %i\n", queue[out]);
+        out= (out+1)%capacity;
+        pthread_mutex_unlock( &mutex );
+        sem_post(&empty);  //Signal empty
     }
-    return min;
 }
+int main(){
+    printf("Enter the size of the buffer: ");
+    scanf("%d", &capacity);
+    printf("Enter the number of items to produce: ");
+    scanf("%d", &max);
+    queue = malloc((capacity) *sizeof(int));
 
-void SJF()
-{
-    Process *p = Entry();
-    int n = 0;
-    while (p[n].pid != -1)
-        n++;
-    int timer = 0;
-    float TAT = 0;
-    float WAT = 0;
-    int done = 0;
-    while (done != n)
-    {
-        int i = min(p, n, timer);
-        int start = timer;
-        int j = i;
-        int ran = p[i].rem_time;
-        while (j == i)
-        {
-            timer++;
-            p[i].rem_time--;
-            j = min(p, n, timer);
-        }
-        printf("%d -> (%d %d)\n", p[i].pid, start, timer);
-        if (p[i].rem_time == 0)
-        {
-            TAT += (timer - p[i].arrival_time);
-            WAT += (timer - p[i].arrival_time - p[i].burst_time);
-            done++;
-        }
-    }
-    TAT = TAT / n;
-    WAT = WAT / n;
-    printf("Turnaround Time = %0.2f ms \n", TAT);
-    printf("Waiting Time = %0.2f ms\n\n\n", WAT);
-}
-
-void RR()
-{
-    Process *p = Entry();
-    int n = 0;
-    while (p[n].pid != -1)
-        n++;
-    int q;
-    printf("Enter time slice:");
-    scanf("%d", &q);
-    int timer = 0;
-    int done = 0;
-    float TAT = 0;
-    float WAT = 0;
-    while (done != n)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            int count = q;
-            int start = timer;
-            while (p[i].rem_time > 0 && p[i].arrival_time <= timer && count > 0)
-            {
-                timer++;
-                p[i].rem_time--;
-                count--;
-            }
-            if (start != timer)
-            {
-                printf("%d -> (%d %d)\n", p[i].pid, start, timer);
-                if (p[i].rem_time == 0)
-                {
-                    TAT += (timer - p[i].arrival_time);
-                    WAT += (timer - p[i].arrival_time - p[i].burst_time);
-                    ;
-                    done++;
-                }
-            }
-        }
-    }
-    TAT = TAT / n;
-    WAT = WAT / n;
-    printf("Turnaround Time = %0.2f ms \n", TAT);
-    printf("Waiting Time = %0.2f ms\n\n\n", WAT);
-}
-
-int comparator(const void *p, const void *q)
-{
-    return ((Process *)p)->arrival_time > ((Process *)q)->arrival_time && ((Process *)p)->priority > ((Process *)q)->priority;
-}
-
-void Prior()
-{
-    Process *p = Entry();
-    int n = 0;
-    while (p[n].pid != -1)
-        n++;
-    // Function to sort based on Priority( if process has arrived)
-    qsort(p, n, sizeof(Process), comparator);
-    int timer = 0;
-    float TAT = 0;
-    float WAT = 0;
-    for (int i = 0; i < n; i++)
-    {
-        WAT += (timer - p[i].arrival_time);
-        printf("%d -> (%d %d)\n", p[i].pid, timer, timer + p[i].burst_time);
-        timer += p[i].burst_time;
-        TAT += (timer - p[i].arrival_time);
-    }
-    TAT = TAT / n;
-    WAT = WAT / n;
-    printf("Turnaround Time = %0.2f ms \n", TAT);
-    printf("Waiting Time = %0.2f ms\n\n\n", WAT);
-}
-
-int main()
-{
-    int ch;
-    while (1)
-    {
-        printf("1. Preemptive SJF\n");
-        printf("2. Round Robin Scheduling\n");
-        printf("3. Priority Scheduling\n");
-        printf("-1 to Exit\n");
-        scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            SJF();
-            break;
-        case 2:
-            RR();
-            break;
-        case 3:
-            Prior();
-            break;
-        case -1:
-            exit(1);
-        default:
-            printf("Invalid Entry\n");
-        }
-    }
+    pthread_t threads[2];
+    sem_init( &full, 0, 0 );
+    sem_init( &empty, 0, capacity);
+    pthread_create( &threads[0], 0, producer, 0 );
+    pthread_create( &threads[1], 0, consumer, 0 );
+    pthread_join( threads[0], 0 );
+    pthread_join( threads[1], 0 );
+    sem_destroy( &full );
+    sem_destroy( &empty );
 }
